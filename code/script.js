569cfa9246e7bbac22c1d5f77d87ebb49d2b0eee
Julien Dargelos <julien.dargelos@me.com>
2017-12-19 21:35:31 +0100

    Tried my best to make things aligned

--- code/script.js
+++ code/script.js
@@ -172,16 +172,13 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
+
+    if (n > slides.length) slideIndex = 1
+    if (n < 1) slideIndex = slides.length
+
+    for (i = 0; i < slides.length; i++) slides[i].style.display = "none";
+
+    if(slideIndex < slides.length - 1) slides[slideIndex - 1].style.display = "block";
 }
 
 window.addEventListener('load', function () {
